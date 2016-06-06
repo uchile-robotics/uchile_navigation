e973c2da41fefdd77346f67fcca835e6da0eda25
matias pavez <matias.pavez.b@gmail.com>
2016-06-06 05:43:00 -0400

    actualizo instalador de bender_nav

--- bender_nav/install/install.sh
+++ bender_nav/install/install.sh
@@ -4,9 +4,6 @@
 # # # # # # # # # # # # # # # # # # # #
 
 # Useful Variables
-pkg_name="bender_nav";
-install_path=$(rospack find $pkg_name)/install;
-install_files=$install_path/files;
 installer="[INSTALLER]:"
 
 # - - - - - - I N S T A L L - - - - - -
@@ -15,20 +12,16 @@ installer="[INSTALLER]:"
 # - - - - - - - Install dependencies - - - - - - - -
 echo -e "\n$installer Installing bender_nav dependencies . . .\n"
 
-bash $(rospack find bender_sensors)/install/install.sh
+bender_cd  bender_sensors
+bash install/install.sh
 
 
 # - - - - - - - - - ROS packages  - - - - - - - - - - -
 echo -e "\n$installer Installing navigation packages . . .\n"
-sudo apt-get install ros-indigo-common-msgs ;
-sudo apt-get install ros-indigo-slam-gmapping ;
-sudo apt-get install ros-indigo-geometry ;
-sudo apt-get install ros-indigo-navigation ;
+sudo apt-get install ros-indigo-common-msgs 
+sudo apt-get install ros-indigo-slam-gmapping 
+sudo apt-get install ros-indigo-geometry 
+sudo apt-get install ros-indigo-navigation 
 
-
-#  - - - - - - - - - Build Navigation - - - - - - - - - - 
-echo -e "\n$installer Building bender_navigation\n";
-cd $BENDER_WORKSPACE/..
-catkin_make --only-pkg-with-deps bender_nav
 
 # :)
