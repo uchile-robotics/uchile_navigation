7af2438e6aecb1348cad835d6ad53126ccbb8ee2
michelleval-uchile <michelleepaz99@gmail.com>
2022-10-25 17:40:27 -0300

    added word_goal

--- uchile_nav/src/test.py
+++ uchile_nav/src/test.py
@@ -12,8 +12,8 @@ def main():
     m.set_pose(x,y,w) 
     m.go()
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
 '''
 if __name__ == '__main__':
     rospy.init_node('fish')
@@ -22,3 +22,24 @@ if __name__ == '__main__':
     #m.get_pose()
     m.go()
 '''
+
+
+def main_w():
+    rospy.init_node('fish')
+    rooms = {"0": (2.561, 0.068), "1": (5.729, 1.341), "2": (6.114, 3.559), "3":(1.853, 3.303)}
+    m = Move()
+    w=1
+    print("where would you like to go? (please select as number)")
+    print("avaliable: 0=hall, 1=living, 2=kitchen, 3=bedroom")
+    place = input()
+    place = str(place)
+    print("you choose:"+place)
+    room = rooms.get(place, rooms["0"])
+    #default es el hall
+    m.set_pose(room[0], room[1], w)
+    m.go()    
+
+
+if __name__ == '__main__':
+    #main()
+    main_w()
